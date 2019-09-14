6e343ae80261b437786eb78a36283926f50a06b8
Oliver <oliv5773@stud.kea.dk>
2019-09-14 23:53:15 +0200

    Initial commit - html and css properties created

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//DOM content is loaded and the colorChanger-function activates:
+document.addEventListener("DOMContentLoaded", colorChanger);
+
+//Main function to allow color changing:
+function colorChanger() {}
