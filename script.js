592b0c4e46dc066886f52efea9856971abdcc782
Oliver <oliv5773@stud.kea.dk>
2019-09-15 02:25:43 +0200

    JS added - the display box updates color based on colorselector-value. Something resembling hex, rgb and hsl is shown - although the values seem to be incorrect. They also don't show until color change #2 for some reason. I guess values given in HSL could visually benefit from being rounded up, but... it is what it is.

--- script.js
+++ script.js
@@ -5,12 +5,26 @@ document.addEventListener("DOMContentLoaded", colorChanger);
 
 //Main function to allow color changing:
 function colorChanger() {
-
     //Variable that lets the larger box change color depending on input-value:
     let selectedColor = document.querySelector(".selected").value;
 
     //Variable that will let the RGB-code displayable:
     let rgbColors = document.querySelector(".display").style.background;
+
+    //Constants and variables declaring how to display the RGB-code:
+    const firstBracket = rgbColors.indexOf("(");
+    const firstComma = rgbColors.indexOf(",");
+    const secondComma = rgbColors.indexOf(",", firstComma + 1);
+    const lastBracket = rgbColors.indexOf(")", secondComma + 1);
+
+    //"r" is declared as "( + first number + ,"
+    let r = rgbColors.substring(firstBracket + 1, firstComma);
+
+    //"g" is declared as ", + second number + ,"
+    let g = rgbColors.substring(firstComma + 2, secondComma);
+
+    //"b" is declared as ", + third number + )"
+    let b = rgbColors.substring(secondComma + 3, lastBracket);
 
     //-----------------------------------------
     //Code given from the assignment in Fronter - makes the translation from RGB to HSL:
@@ -49,4 +63,21 @@ function colorChanger() {
     s *= 100;
     l *= 100;
     //-----------------------------------------
+
+    //The larger box is selected and the background is changed according to the variable-values:
+    document.querySelector(".display").style.background = selectedColor;
+
+    //the HEX code for the selected color is displayed:
+    document.querySelector(".hex").innerHTML = `HEX:
+    ${selectedColor}`;
+
+    //the RGB code for the selected color is displayed:
+    document.querySelector(".rgb").innerHTML = `RGB:
+    ${rgbColors}`;
+
+    //the HSL code for the selected color is displayed:
+    document.querySelector(".hsl").innerHTML = `HSL:
+    ${h},
+    ${s}%,
+    ${l}%`;
 }
